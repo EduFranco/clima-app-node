d624fb4c33910fb8d75e4322fb99a7bd5e27a320
Edu Franco <eduardo.ezequiel.franco@gmail.com>
2019-12-06 14:48:32 -0300

    Primer commit

---
+++ lugar/lugar.js
@@ -0,0 +1,32 @@
+const axios = require('axios');
+
+
+const getLugarLatLng = async(dir) => {
+    const encodeUlr = encodeURI(dir);
+
+    const instance = axios.create({
+        baseURL: `https://devru-latitude-longitude-find-v1.p.rapidapi.com/latlon.php?location=${encodeUlr}`,
+        headers: { 'x-rapidapi-key': 'c3eb518bb9msh4090f2d30df2487p17b592jsn13a6ffc144c7' }
+    });
+
+    const resp = await instance.get();
+
+    if (resp.data.Results.length === 0) {
+        throw new Error(`No hay resultados para ${dir}`);
+    }
+
+    const data = resp.data.Results[0];
+    const direccion = data.name;
+    const lat = data.lat;
+    const lng = data.lon;
+
+    return {
+        direccion,
+        lat,
+        lng
+    };
+};
+
+module.exports = {
+    getLugarLatLng
+};
